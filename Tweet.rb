667e38ad86a7715c8de28353300f738b04ac22a4
Andy Tiffany <andy.tiffany@twintechs.com>
2015-11-05 19:25:34 -0800

    adds twitter API class and Tweet class

---
+++ Tweet.rb
@@ -0,0 +1,8 @@
+class Tweet
+  attr_accessor :id, :text
+
+  def initialize(id, text)
+    @id = id
+    @text = text
+  end
+end
---
+++ TwitterAPI.rb
@@ -0,0 +1,32 @@
+require 'twitter'
+require './Tweet.rb'
+
+class TwitterAPI
+
+  CONSUMER_KEY = "YOUR KEY HERE"
+  CONSUMER_SECRET = "YOUR SECRET HERE"
+  ACCESS_TOKEN = "YOUR KEY HERE"
+  ACCESS_TOKEN_SECRET = "YOUR SECRET HERE"
+
+  attr_accessor :client
+
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = CONSUMER_KEY
+      config.consumer_secret     = CONSUMER_SECRET
+      config.access_token        = ACCESS_TOKEN
+      config.access_token_secret = ACCESS_TOKEN_SECRET
+    end
+  end
+
+  def search(pattern, since_id=0)
+    tweets = []
+
+    client.search(pattern, { :since_id => since_id }).take(30).each do |tweet|
+      puts "adding tweet: #{tweet.id}"
+      tweets << Tweet.new(tweet.id, tweet.text)
+    end
+
+    return tweets
+  end
+end
